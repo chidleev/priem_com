d2f1a4917fc5a4ec1447f9058ad3ea2c9c3d1194
chidleev <chidleev22@gmail.com>
2022-06-23 13:00:56 +0300

    654654

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const path = require('path')
 
 const express = require('express')
 
-const dataBase = require('./dataBase')
-
 const serverApp = express()
 serverApp.locals.PORT = 3000
 
@@ -14,12 +12,7 @@ serverApp.use('/scripts', express.static(path.join(__dirname, 'node_modules')))
 
 serverApp.use(express.static(path.join(__dirname, 'public')))
 
-dataBase.client.sync()
-    .then(() => {
-        serverApp.listen(serverApp.locals.PORT, () => {
-            console.log(`Server running on port ${serverApp.locals.PORT}`)
-        })
-    })
-    .catch(error => {
-        console.error(error);
-    })
+
+serverApp.listen(serverApp.locals.PORT, () => {
+    console.log(`Server running on port ${serverApp.locals.PORT}`)
+})
---
+++ start.cmd
@@ -0,0 +1,1 @@
+start chrome "http://localhost:3000/index.html" && npm start
